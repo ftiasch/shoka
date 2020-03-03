42f85f064099730a2c089409b37a887cf2c24eab
Xiaoxu Guo <ftiasch0@gmail.com>
2020-03-03 12:20:20 +0800

    added rand parenthesis

--- gen.h
+++ gen.h
@@ -11,3 +11,22 @@ template <typename Int> static std::set<Int> rand_subset(Int n, int k) {
   }
   return set;
 }
+
+static std::string rand_parenthesis(int n) {
+  std::vector<bool> bits((n << 1) + 1);
+  for (int i = 0; i <= n; ++i) {
+    bits[i] = 1;
+  }
+  shuffle(bits.begin(), bits.end());
+  int sum = 0;
+  std::pair<int, int> best{0, 0};
+  for (int i = 0; i <= n << 1; ++i) {
+    sum += bits[i] ? -1 : 1;
+    best = std::min(best, std::make_pair(sum, i + 1));
+  }
+  std::string result;
+  for (int i = 0; i < n << 1; ++i) {
+    result += bits[(best.second + i) % ((n << 1) + 1)] ? ')' : '(';
+  }
+  return result;
+}
