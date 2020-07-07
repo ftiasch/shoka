6e1ea4c74140847b045569ea649ff7f111088aae
Xiaoxu Guo <ftiasch0@gmail.com>
2020-07-07 23:25:25 +0800

    added union find set

---
+++ union_find.h
@@ -0,0 +1,27 @@
+class UnionFindSet {
+public:
+  UnionFindSet(int n) : parent(n, -1) {}
+
+  int find(int u) {
+    if (!~parent[u]) {
+      return u;
+    }
+    if (parent[u] != u) {
+      parent[u] = find(parent[u]);
+    }
+    return parent[u];
+  }
+
+  bool merge(int a, int b) {
+    if (find(a) == find(b)) {
+      return false;
+    }
+    parent[find(a)] = find(b);
+    return true;
+  }
+
+private:
+  std::vector<int> parent;
+};
+
+using UFS = UnionFindSet;
