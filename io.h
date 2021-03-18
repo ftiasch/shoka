85560c58e9c8efbe93f15d2eb960f32991080d2b
Xiaoxu Guo <ftiasch0@gmail.com>
2021-03-18 15:11:33 +0800

    rewrote

--- io.h
+++ io.h
@@ -1,58 +1,48 @@
 struct IO {
-  IO(FILE *in = stdin) : in(in) {}
+  IO(FILE *in_ = stdin) : in(in_) {}
 
-  int next_char() {
-    if (position == length) {
-      position = 0, length = fread(buffer, 1, LENGTH, in);
+  char next_char() {
+    if (head == length) {
+      head = 0;
+      length = fread(buffer, 1, LENGTH, in);
     }
-    if (position == length) {
-      eof = true;
-      return -1;
-    }
-    return buffer[position++];
+    return buffer[head++];
   }
 
-  int next_uint() {
-    int c = next_char(), x = 0;
-    while (c <= 32) {
+  uint64_t next_uint() {
+    char c = next_char();
+    while (!std::isdigit(c)) {
       c = next_char();
     }
-    for (; '0' <= c && c <= '9'; c = next_char()) {
-      x = x * 10 + c - '0';
+    uint64_t x = 0;
+    for (; std::isdigit(c); c = next_char()) {
+      x = x * 10U + c - '0';
     }
     return x;
   }
 
-  int next_int() {
-    int s = 1, c = next_char(), x = 0;
-    while (c <= 32) {
+  int64_t next_int() {
+    char c = next_char();
+    while (!std::isdigit(c) && c != '-') {
       c = next_char();
     }
+    int64_t sign = 1;
     if (c == '-') {
-      s = -1, c = next_char();
+      sign = -1;
+      c = next_char();
     }
-    for (; '0' <= c && c <= '9'; c = next_char()) {
+    int64_t x = 0;
+    for (; std::isdigit(c); c = next_char()) {
       x = x * 10 + c - '0';
     }
-    return x * s;
-  }
-
-  void next_string(char *s) {
-    int c = next_char();
-    while (c <= 32) {
-      c = next_char();
-    }
-    for (; c > 32; c = next_char()) {
-      *s++ = c;
-    }
-    *s = 0;
+    return sign * x;
   }
 
 private:
   static const int LENGTH = 1 << 16;
 
   char buffer[LENGTH];
-  int position = 0, length = 0;
-  bool eof = false;
+  size_t head = 0;
+  size_t length = 0;
   FILE *in;
 };
