37112f206c8713c7a77e714b7cda06e7d6e69fc3
Xiaoxu Guo <ftiasch0@gmail.com>
2021-07-10 22:26:03 +0800

    added ModT

---
+++ mod.h
@@ -0,0 +1,108 @@
+#include <cstdint>
+#include <limits>
+
+#define DECLARE_NON_CONST_MODT(name)                                           \
+  using name = mod::ModT<0>;                                                   \
+  uint32_t name::MOD
+
+namespace mod {
+
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+template <u32 MOD_> struct ModT {
+  static const u32 MOD = MOD_;
+
+  static_assert(MOD <= (std::numeric_limits<u32>::max() >> 1));
+
+  explicit constexpr ModT(u32 x_ = 0) : x(x_ % MOD) {}
+
+  constexpr u32 get() const { return x; }
+
+  constexpr ModT &operator+=(const ModT &other) {
+    x += other.x;
+    if (x >= MOD) {
+      x -= MOD;
+    }
+    return *this;
+  }
+
+  constexpr ModT operator+(const ModT &other) const {
+    ModT copy = *this;
+    return copy += other;
+  }
+
+  constexpr ModT &operator-=(const ModT &other) {
+    x += MOD - other.x;
+    if (x >= MOD) {
+      x -= MOD;
+    }
+    return *this;
+  }
+
+  constexpr ModT operator-(const ModT &other) const {
+    ModT copy = *this;
+    return copy -= other;
+  }
+
+  constexpr ModT operator*=(const ModT &other) {
+    x = static_cast<u64>(x) * static_cast<u64>(other.x) % MOD;
+    return *this;
+  }
+
+  constexpr ModT operator*(const ModT &other) const {
+    ModT copy = *this;
+    return copy *= other;
+  }
+
+  u32 x;
+};
+
+// TODO: Non-const mod
+template <> struct ModT<0> {
+  static u32 MOD;
+
+  explicit ModT(u32 x_ = 0) : x(x_ % MOD) {}
+
+  u32 get() const { return x; }
+
+  ModT &operator+=(const ModT &other) {
+    x += other.x;
+    if (x >= MOD) {
+      x -= MOD;
+    }
+    return *this;
+  }
+
+  ModT operator+(const ModT &other) const {
+    ModT copy = *this;
+    return copy += other;
+  }
+
+  ModT &operator-=(const ModT &other) {
+    x += MOD - other.x;
+    if (x >= MOD) {
+      x -= MOD;
+    }
+    return *this;
+  }
+
+  ModT operator-(const ModT &other) const {
+    ModT copy = *this;
+    return copy -= other;
+  }
+
+  ModT operator*=(const ModT &other) {
+    x = static_cast<u64>(x) * static_cast<u64>(other.x) % MOD;
+    return *this;
+  }
+
+  ModT operator*(const ModT &other) const {
+    ModT copy = *this;
+    return copy *= other;
+  }
+
+  u32 x;
+};
+
+} // namespace mod
--- test/montgomery.cc
+++ test/montgomery.cc
@@ -1,4 +1,5 @@
 #include "../montgomery.h"
+#include "../mod.h"
 
 #include <random>
 
@@ -8,6 +9,40 @@ static const int MOD = 998244353;
 
 TEST(Montgomery, Test) {
   using ModT = montgomery::Montgomery32T<MOD>;
+
+  std::mt19937_64 gen(0);
+  uint32_t a = gen() % MOD;
+  uint32_t b = gen() % MOD;
+  ModT mz;
+  ModT ma(a);
+  ASSERT_EQ(ma.get(), a);
+  ModT mb(b);
+  ASSERT_EQ(mb.get(), b);
+  ASSERT_EQ((ma + mb).get(), (a + b) % MOD);
+  ModT mc = ma * mb;
+  ASSERT_EQ(mc.get(), (uint64_t)a * b % MOD);
+}
+
+TEST(ModT, Test) {
+  using ModT = mod::ModT<MOD>;
+
+  std::mt19937_64 gen(0);
+  uint32_t a = gen() % MOD;
+  uint32_t b = gen() % MOD;
+  ModT mz;
+  ModT ma(a);
+  ASSERT_EQ(ma.get(), a);
+  ModT mb(b);
+  ASSERT_EQ(mb.get(), b);
+  ASSERT_EQ((ma + mb).get(), (a + b) % MOD);
+  ModT mc = ma * mb;
+  ASSERT_EQ(mc.get(), (uint64_t)a * b % MOD);
+}
+
+DECLARE_NON_CONST_MODT(ModT);
+
+TEST(ModT, NonConstModTest) {
+  ModT::MOD = MOD;
 
   std::mt19937_64 gen(0);
   uint32_t a = gen() % MOD;
