ab84ff7f29c3834f553d4151477370897c2e871e
Xiaoxu Guo <ftiasch0@gmail.com>
2021-07-30 14:43:27 +0800

    added sieve

---
+++ sieve.h
@@ -0,0 +1,26 @@
+#include <vector>
+
+struct PrimeGen : public std::vector<int> {
+  PrimeGen(int m) : min_div_(m) {
+    for (int d = 2; d < m; ++d) {
+      if (min_div_[d] == -1) {
+        min_div_[d] = d;
+        push_back(d);
+      }
+      for (int p : *this) {
+        if (d * p >= m) {
+          break;
+        }
+        min_div_[p * d] = p;
+        if (d % p == 0) {
+          break;
+        }
+      }
+    }
+  }
+
+  int min_div(int n) const { return min_div_[n]; }
+
+private:
+  std::vector<int> min_div_;
+};
