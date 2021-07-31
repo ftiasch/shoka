c28e20cbfa3cd170491e5e776d9d164d037ad266
Xiaoxu Guo <ftiasch0@gmail.com>
2021-07-31 21:30:54 +0800

    improved

--- heavy_light_decomp.h
+++ heavy_light_decomp.h
@@ -1,13 +1,23 @@
+#include <algorithm>
+#include <vector>
+
 template <typename NestedT> struct HeavyLightDecomposition {
   using Tree = std::vector<std::vector<int>>;
 
   explicit HeavyLightDecomposition(int n, const Tree &tree, int root)
-      : path(n, nullptr), depth(n), lowest(n), highest(n) {
-    build(tree, -1, root);
+      : path(n, nullptr), parent(n), depth(n), lowest(n), highest(n) {
+    parent[root] = -1;
+    build(tree, root);
     for (int u = 0; u < n; ++u) {
       lowest[u] = lowest[highest[u]];
       if (highest[u] == u) {
-        path[u] = new NestedT{depth[u] - get_lowest_depth(u)};
+        const int size = depth[u] - get_lowest_depth(u);
+        std::vector<int> vertices;
+        vertices.reserve(size);
+        for (int i = 0, v = u; i < size; ++i, v = parent[v]) {
+          vertices.push_back(v);
+        }
+        path[u] = new NestedT{vertices};
       }
     }
   }
@@ -20,35 +30,56 @@ template <typename NestedT> struct HeavyLightDecomposition {
     }
   }
 
-  template <typename Result> Result query(bool include_lca, int a, int b) {
-    Result result;
+  template <typename Handler>
+  Handler traverse(Handler &&h, bool include_lca, int a, int b) {
     while (highest[a] != highest[b]) {
-      int &p = get_lowest_depth(a) > get_lowest_depth(b) ? a : b;
-      int base = depth[highest[p]];
-      result.update(path[highest[p]], base - depth[p],
-                    base - get_lowest_depth(p) - 1);
-      p = lowest[p];
+      if (get_lowest_depth(a) > get_lowest_depth(b)) {
+        const int base = depth[highest[a]];
+        h.update<0>(path[highest[a]], base - depth[a],
+                    base - get_lowest_depth(a) - 1);
+        a = lowest[a];
+      } else {
+        const int base = depth[highest[b]];
+        h.update<1>(path[highest[b]], base - depth[b],
+                    base - get_lowest_depth(b) - 1);
+        b = lowest[b];
+      }
     }
     if (include_lca || a != b) {
-      if (depth[a] < depth[b]) {
-        std::swap(a, b);
-      }
-      // depth[a] >= depth[b]
-      int base = depth[highest[a]];
-      result.update(path[highest[a]], base - depth[a],
+      if (depth[a] > depth[b]) {
+        const int base = depth[highest[a]];
+        h.update<0>(path[highest[a]], base - depth[a],
                     base - depth[b] - (!include_lca));
+      } else {
+        const int base = depth[highest[b]];
+        h.update<1>(path[highest[b]], base - depth[b],
+                    base - depth[a] - (!include_lca));
+      }
     }
-    return result;
+    return h;
+  }
+
+  int lca(int a, int b) const {
+    while (highest[a] != highest[b]) {
+      if (get_lowest_depth(a) > get_lowest_depth(b)) {
+        a = lowest[a];
+      } else {
+        b = lowest[b];
+      }
+    }
+    return depth[a] > depth[b] ? b : a;
   }
 
 private:
-  int build(const Tree &tree, int p, int u) {
+  int build(const Tree &tree, int u) {
+    const int p = parent[u];
     depth[u] = ~p ? depth[p] + 1 : 0;
     int size = 1;
     std::pair<int, int> candidate{0, u};
     for (int v : tree[u]) {
       if (v != p) {
-        int subtree_size = build(tree, u, v);
+        parent[v] = u;
+        int subtree_size = build(tree, v);
         size += subtree_size;
         candidate =
             std::max(candidate, std::make_pair(subtree_size, highest[v]));
@@ -64,5 +95,5 @@ private:
   }
 
   std::vector<NestedT *> path;
-  std::vector<int> depth, lowest, highest;
+  std::vector<int> parent, depth, lowest, highest;
 };
