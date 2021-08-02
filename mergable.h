96ed7afcf49e4c91724bc92111057a07c1428aa4
Xiaoxu Guo <ftiasch0@gmail.com>
2021-08-03 02:51:38 +0800

    simplified

--- mergable.h
+++ mergable.h
@@ -7,19 +7,9 @@
 
 namespace details {
 
-struct MergableNodePointer {
-  explicit MergableNodePointer(int p_ = 0) : p(p_) {}
-
-  explicit operator int() const { return p; }
-
-  int unwrap() const { return p; }
-
-  int p;
-};
-
 template <typename NodeBase> struct MergableNodeT : public NodeBase {
   int size;
-  MergableNodePointer left_child, right_child;
+  MergableNodeT *left_child, *right_child;
   uint32_t rank;
 };
 
@@ -27,83 +17,71 @@ template <typename NodeBase> struct MergableNodeT : public NodeBase {
 
 template <typename NodeBase, typename Impl> struct MergableTreapBase {
   using Node = details::MergableNodeT<NodeBase>;
-  using Pointer = details::MergableNodePointer;
 
   explicit MergableTreapBase(int n_) : number_of_node(1), nodes(n_) {
     std::mt19937 gen;
     for (auto &n : nodes) {
       n.rank = gen();
     }
-    Node *n = nodes.data();
+    Node *n = null_node();
     n->rank = std::numeric_limits<uint32_t>::max();
     n->size = 0;
-    n->left_child = n->right_child = Pointer(0);
+    n->left_child = n->right_child = null_node();
   }
 
-  static Pointer null_pointer() { return Pointer(0); }
+  Node *null_node() { return nodes.data(); }
 
-  Pointer new_node() {
-    Pointer p(number_of_node++);
-    Node *n = get_node(p);
+  Node *new_node() {
+    Node *n = nodes.data() + (number_of_node++);
     n->size = 1;
-    n->left_child = n->right_child = null_pointer();
-    return p;
+    n->left_child = n->right_child = null_node();
+    return n;
   }
 
-  Node *get_node(Pointer p) { return nodes.data() + p.unwrap(); }
-
-  std::pair<Pointer, Pointer> split(Pointer pn, int k) {
+  std::pair<Node *, Node *> split(Node *n, int k) {
     if (k == 0) {
-      return {Pointer(0), pn};
+      return {null_node(), n};
     }
-    Node *n = get_node(pn);
     if (k == n->size) {
-      return {pn, Pointer(0)};
+      return {n, null_node()};
     }
-    Impl::propagate(n, get_node(n->left_child), get_node(n->right_child));
-    Node *ln = get_node(n->left_child);
-    Node *rn = get_node(n->right_child);
+    Impl::propagate(n);
+    Node *ln = n->left_child;
+    Node *rn = n->right_child;
     if (k <= ln->size) {
       auto result = split(n->left_child, k);
-      collect(n, get_node(n->left_child = result.second), rn);
-      return {result.first, pn};
+      n->left_child = result.second;
+      return {result.first, collect(n)};
     } else {
       auto result = split(n->right_child, k - 1 - ln->size);
-      collect(n, ln, get_node(n->right_child = result.first));
-      return {pn, result.second};
+      n->right_child = result.first;
+      return {collect(n), result.second};
     }
   }
 
-  Pointer merge(Pointer pu, Pointer pv) {
-    if (pu.unwrap() == 0) {
-      return pv;
+  Node *merge(Node *u, Node *v) {
+    if (u == null_node()) {
+      return v;
     }
-    if (pv.unwrap() == 0) {
-      return pu;
+    if (v == null_node()) {
+      return u;
     }
-    Node *u = get_node(pu);
-    Node *v = get_node(pv);
     if (u->rank < v->rank) {
-      Impl::propagate(u, get_node(u->left_child), get_node(u->right_child));
-      u->right_child = merge(u->right_child, pv);
-      collect(u, get_node(u->left_child), get_node(u->right_child));
-      return pu;
+      Impl::propagate(u);
+      u->right_child = merge(u->right_child, v);
+      return collect(u);
     } else {
-      Impl::propagate(v, get_node(v->left_child), get_node(v->right_child));
-      v->left_child = merge(pu, v->left_child);
-      collect(v, get_node(v->left_child), get_node(v->right_child));
-      return pv;
+      Impl::propagate(v);
+      v->left_child = merge(u, v->left_child);
+      return collect(v);
     }
   }
 
 private:
-  Pointer get_pointer(const Node *n) const {
-    return NodePonter(n - nodes.data());
-  }
-
-  static void collect(Node *n, const Node *ln, const Node *rn) {
-    Impl::collect(n, ln, rn);
-    n->size = ln->size + 1 + rn->size;
+  static Node *collect(Node *n) {
+    Impl::collect(n);
+    n->size = n->left_child->size + 1 + n->right_child->size;
+    return n;
   }
 
   int number_of_node;
