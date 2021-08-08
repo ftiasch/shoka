7bfc4739748131f3fbfa74d421a5f341e636d6b4
Xiaoxu Guo <ftiasch0@gmail.com>
2021-08-08 08:14:33 -0700

    added scc_graph

--- tarjan.h
+++ tarjan.h
@@ -2,6 +2,7 @@
 #include <vector>
 
 class Tarjan {
+public:
   using Graph = std::vector<std::vector<int>>;
 
   static Graph to_graph(int n, const std::vector<std::pair<int, int>> &edges) {
@@ -12,28 +13,37 @@ class Tarjan {
     return graph;
   }
 
-public:
-  Tarjan(int n_, const Graph &graph) : n(n_), dfn(n_, -1), scc_id(n_), low(n_) {
+  Tarjan(int n_, const Graph &graph_)
+      : n(n_), dfn(n, -1), scc_id(n), low(n), graph(graph_) {
     int dfs_count = 0;
     std::vector<int> stack;
     for (int r = 0; r < n_; ++r) {
-      dfs(graph, dfs_count, stack, r);
+      dfs(dfs_count, stack, r);
     }
   }
 
-  Tarjan(int n, const std::vector<std::pair<int, int>> &edges)
-      : Tarjan(n, to_graph(n, edges)) {}
+  Graph scc_graph() const {
+    Graph result(number_of_scc);
+    for (int u = 0; u < n; ++u) {
+      for (int v : graph[u]) {
+        if (scc_id[u] != scc_id[v]) {
+          result[scc_id[u]].push_back(scc_id[v]);
+        }
+      }
+    }
+    return result;
+  }
 
-  int number_of_scc = 0;
+  int n, number_of_scc = 0;
   std::vector<int> dfn, scc_id;
 
 private:
-  void dfs(const Graph &graph, int &dfs_count, std::vector<int> &stack, int u) {
+  void dfs(int &dfs_count, std::vector<int> &stack, int u) {
     if (dfn[u] == -1) {
       int tmp = dfn[u] = low[u] = dfs_count++;
       stack.push_back(u);
       for (int v : graph[u]) {
-        dfs(graph, dfs_count, stack, v);
+        dfs(dfs_count, stack, v);
         tmp = std::min(tmp, low[v]);
       }
       low[u] = tmp;
@@ -50,6 +60,6 @@ private:
     }
   }
 
-  int n;
   std::vector<int> low;
+  const Graph &graph;
 };
