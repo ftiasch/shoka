eaafd125ed3e15312d46069c81c48409144d99c2
Xiaoxu Guo <ftiasch0@gmail.com>
2021-09-02 21:30:21 +0800

    refactored HLD

--- heavy_light_decomp.h
+++ heavy_light_decomp.h
@@ -1,12 +1,11 @@
 #include <algorithm>
 #include <vector>
 
-template <typename NestedT> struct HeavyLightDecomposition {
+struct HeavyLightDecompositionBase {
   using Tree = std::vector<std::vector<int>>;
 
-  HeavyLightDecomposition(const Tree &tree, int root)
-      : n(tree.size()), parent(n), size(n), depth(n), path(n, nullptr),
-        lowest(n), highest(n) {
+  HeavyLightDecompositionBase(const Tree &tree, int root)
+      : n(tree.size()), parent(n), size(n), depth(n), lowest(n), highest(n) {
     parent[root] = -1;
     build(tree, root);
     for (int u = 0; u < n; ++u) {
@@ -14,7 +13,65 @@ template <typename NestedT> struct HeavyLightDecomposition {
     }
   }
 
-  void init_biased_nested() {
+  int lca(int a, int b) const {
+    while (highest[a] != highest[b]) {
+      if (get_lowest_depth(a) > get_lowest_depth(b)) {
+        a = lowest[a];
+      } else {
+        b = lowest[b];
+      }
+    }
+    return depth[a] < depth[b] ? a : b;
+  }
+
+  int n;
+  std::vector<int> parent, size, depth;
+
+protected:
+  void build(const Tree &tree, int u) {
+    const int p = parent[u];
+    depth[u] = ~p ? depth[p] + 1 : 0;
+    size[u] = 1;
+    std::pair<int, int> candidate{0, u};
+    for (int v : tree[u]) {
+      if (v != p) {
+        parent[v] = u;
+        build(tree, v);
+        size[u] += size[v];
+        candidate = std::max(candidate, std::make_pair(size[v], highest[v]));
+      }
+    }
+    highest[u] = candidate.second;
+    lowest[highest[u]] = p;
+  }
+
+  int get_lowest_depth(int u) const {
+    return ~lowest[u] ? depth[lowest[u]] : -1;
+  }
+
+  std::vector<int> lowest, highest;
+};
+
+template <typename NestedT>
+struct HeavyLightDecomposition : public HeavyLightDecompositionBase {
+  HeavyLightDecomposition(const Tree &tree, int root)
+      : HeavyLightDecompositionBase(tree, root), path(n, nullptr) {}
+
+  void init() {
+    for (int u = 0; u < n; ++u) {
+      if (highest[u] == u) {
+        const int count = depth[u] - get_lowest_depth(u);
+        std::vector<int> vertices, weight;
+        vertices.reserve(count);
+        for (int i = 0, v = u; i < count; ++i, v = parent[v]) {
+          vertices.push_back(v);
+        }
+        path[u] = new NestedT(vertices);
+      }
+    }
+  }
+
+  void init_biased() {
     for (int u = 0; u < n; ++u) {
       if (highest[u] == u) {
         const int count = depth[u] - get_lowest_depth(u);
@@ -70,42 +127,6 @@ template <typename NestedT> struct HeavyLightDecomposition {
     return h;
   }
 
-  int lca(int a, int b) const {
-    while (highest[a] != highest[b]) {
-      if (get_lowest_depth(a) > get_lowest_depth(b)) {
-        a = lowest[a];
-      } else {
-        b = lowest[b];
-      }
-    }
-    return depth[a] < depth[b] ? a : b;
-  }
-
-  int n;
-  std::vector<int> parent, size, depth;
-
 private:
-  void build(const Tree &tree, int u) {
-    const int p = parent[u];
-    depth[u] = ~p ? depth[p] + 1 : 0;
-    size[u] = 1;
-    std::pair<int, int> candidate{0, u};
-    for (int v : tree[u]) {
-      if (v != p) {
-        parent[v] = u;
-        build(tree, v);
-        size[u] += size[v];
-        candidate = std::max(candidate, std::make_pair(size[v], highest[v]));
-      }
-    }
-    highest[u] = candidate.second;
-    lowest[highest[u]] = p;
-  }
-
-  int get_lowest_depth(int u) const {
-    return ~lowest[u] ? depth[lowest[u]] : -1;
-  }
-
   std::vector<NestedT *> path;
-  std::vector<int> lowest, highest;
 };
