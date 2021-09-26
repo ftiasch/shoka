f922dc52b6b5fd89dd8988beb1dacc01e83bc50e
Xiaoxu Guo <ftiasch0@gmail.com>
2021-09-26 12:43:50 +0800

    split ST

---
+++ sparse_table.h
@@ -0,0 +1,32 @@
+#include <algorithm>
+
+template <typename T, int N> struct SparseTable {
+  SparseTable() {
+    log[1] = 0;
+    for (int i = 2; i <= N; ++i) {
+      log[i] = 31 - __builtin_clz(i - 1);
+    }
+  }
+
+  void compute(int n, const T *value) {
+    l = log2n(n), memcpy(st[0], value, sizeof(T) * n);
+    for (int i = 1; i < l; ++i) {
+      for (int j = 0; j + (1 << i) <= n; ++j) {
+        st[i][j] = std::min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+      }
+    }
+  }
+
+  T rmq(int a, int b) const {
+    const int l = log[b - a];
+    return std::min(st[l][a], st[l][b - (1 << l)]);
+  }
+
+private:
+  static constexpr int log2n(int n) { return 32 - __builtin_clz(n - 1); }
+
+  static const int L = log2n(N);
+
+  int l, log[N + 1];
+  T st[L][N];
+};
