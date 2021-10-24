6dcb0bd84787c0b73e04cae99549a46ad01ff837
Xiaoxu Guo <ftiasch0@gmail.com>
2021-10-25 00:29:50 +0800

    refactored prefix table

--- prefix_table.h
+++ prefix_table.h
@@ -1,19 +1,17 @@
 #include <algorithm>
 
-template <int N> struct PrefixTable {
-  void compute(int n, const char *s) {
-    z[0] = 0;
-    for (int i = 1, j = 0; i < n; ++i) {
-      z[i] = i < j + z[j] ? std::min(z[i - j], j + z[j] - i) : 0;
-      while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
-        z[i]++;
-      }
-      if (i + z[i] > j + z[j]) {
-        j = i;
-      }
+std::vector<int> prefix_table(int n, const char *s) {
+  std::vector<int> z(n);
+  z[0] = 0;
+  for (int i = 1, j = 0; i < n; ++i) {
+    z[i] = i < j + z[j] ? std::min(z[i - j], j + z[j] - i) : 0;
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+      z[i]++;
     }
-    z[0] = n;
+    if (i + z[i] > j + z[j]) {
+      j = i;
+    }
   }
-
-  int z[N];
-};
+  z[0] = n;
+  return z;
+}
