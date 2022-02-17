26326619d0abd6377669d48616b81a7e96fc7536
Xiaoxu Guo <ftiasch0@gmail.com>
2022-02-17 14:14:44 +0800

    added fixed size matrix

---
+++ fixed_size_matrix.h
@@ -0,0 +1,56 @@
+#include <array>
+#include <cstring>
+#include <vector>
+
+template <typename T, size_t N> struct FixedSizeMatrixT {
+  FixedSizeMatrixT() { memset(data, 0, sizeof(data)); }
+
+  T *operator[](int i) { return data[i]; }
+
+  const T *operator[](int i) const { return data[i]; }
+
+  FixedSizeMatrixT &operator+=(const FixedSizeMatrixT &o) {
+    for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < N; ++j) {
+        data[i][j] += o[i][j];
+      }
+    }
+    return *this;
+  }
+
+  FixedSizeMatrixT operator+(const FixedSizeMatrixT &o) {
+    return FixedSizeMatrixT(*this) += o;
+  }
+
+  FixedSizeMatrixT &operator-=(const FixedSizeMatrixT &o) {
+    for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < N; ++j) {
+        data[i][j] -= o[i][j];
+      }
+    }
+    return *this;
+  }
+
+  FixedSizeMatrixT operator-(const FixedSizeMatrixT &o) {
+    return FixedSizeMatrixT(*this) -= o;
+  }
+
+  FixedSizeMatrixT operator*(const FixedSizeMatrixT &o) {
+    static T buffer[N];
+    FixedSizeMatrixT result;
+    for (int j = 0; j < N; ++j) {
+      for (int i = 0; i < N; ++i) {
+        buffer[i] = o[i][j];
+      }
+      for (int i = 0; i < N; ++i) {
+        for (int k = 0; k < N; ++k) {
+          result[i][j] += data[i][k] * buffer[k];
+        }
+      }
+    }
+    return result;
+  }
+
+private:
+  T data[N][N];
+};
