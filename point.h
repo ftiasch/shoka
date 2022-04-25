32d69a64b9fbfaf5c57fbf5d388075b13a4022f6
Xiaoxu Guo <ftiasch0@gmail.com>
2022-04-25 23:17:02 +0800

    added point

---
+++ point.h
@@ -0,0 +1,33 @@
+template <typename PT = int, typename P2T = long long> struct PointT {
+  PointT &operator+=(const PointT &o) {
+    x += o.x;
+    y += o.y;
+    return *this;
+  }
+
+  PointT operator+(const PointT &o) const {
+    PointT copy = *this;
+    return copy += o;
+  }
+
+  PointT &operator-=(const PointT &o) {
+    x -= o.x;
+    y -= o.y;
+    return *this;
+  }
+
+  PointT operator-(const PointT &o) const {
+    PointT copy = *this;
+    return copy -= o;
+  }
+
+  P2T dot(const PointT &o) const {
+    return static_cast<P2T>(x) * o.x + static_cast<P2T>(y) * o.y;
+  }
+
+  P2T det(const PointT &o) const {
+    return static_cast<P2T>(x) * o.y - static_cast<P2T>(y) * o.x;
+  }
+
+  PT x, y;
+};
