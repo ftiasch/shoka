a33df55e434ebd8b4df02f915fd299026d405572
Xiaoxu Guo <ftiasch0@gmail.com>
2022-04-25 23:16:49 +0800

    added util

---
+++ util.h
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define CIN(T, V)                                                              \
+  T V;                                                                         \
+  std::cin >> V;
+
+#define ALL(v) (v).begin(), (v).end()
+
+template <typename T> std::vector<std::vector<T>> vector_2d(int n, int m) {
+  return std::vector<std::vector<T>>(n, std::vector<T>(m));
+}
+
+template <typename T> std::vector<std::vector<T>> vector_2d(int n, int m, T d) {
+  return std::vector<std::vector<T>>(n, std::vector<T>(m, d));
+}
+
+using Long = long long;
+using u64 = uint64_t;
+
+template <typename T>
+using MinPQ = std::priority_queue<T, std::vector<T>, std::greater<T>>;
