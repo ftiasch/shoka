e64953a94c7d8f9efd5947dc7bd2bad9b549b336
hly1204 <hly1204@qq.com>
2022-05-16 19:25:46 +0800

    improved speed

--- multi_eval.h
+++ multi_eval.h
@@ -26,15 +26,30 @@ public:
       NTT::dif(2, dif_rev_q[0].data() + (i << 1));
     }
     for (int l = 1; l < log_m; ++l) {
+      ModT inv_n(ModT(1 << l).inverse());
+      ModT G(NTT::get_primitive_root());
+      ModT zeta(G.power(ModT::MOD - 1 - (ModT::MOD >> (l + 1))));
       for (int s = 0; s < (m << 1); s += (2 << l)) {
-        Poly<NTT>::dot_product_and_dit(1 << l, ModT(1 << l).inverse(),
+        if (l + 1 < log_m) {
+          for (int i = s; i < s + (1 << l); ++i) {
+            dif_rev_q[l][i] = dif_rev_q[l - 1][i] * dif_rev_q[l - 1][i + (1 << l)];
+            dif_rev_q[l][i + (1 << l)] = inv_n * dif_rev_q[l][i];
+          }
+          NTT::dit(1 << l, dif_rev_q[l].data() + s + (1 << l));
+          dif_rev_q[l][s + (1 << l)] -= ModT{2};
+          ModT zeta_tmp{1};
+          for (int i = s + (1 << l); i < s + (2 << l); ++i) {
+            dif_rev_q[l][i] *= zeta_tmp;
+            zeta_tmp *= zeta;
+          }
+          NTT::dif(1 << l, dif_rev_q[l].data() + s + (1 << l));
+        } else {
+          Poly<NTT>::dot_product_and_dit(1 << l, inv_n,
                                        dif_rev_q[l].data() + s,
                                        dif_rev_q[l - 1].data() + s,
                                        dif_rev_q[l - 1].data() + s + (1 << l));
-        dif_rev_q[l][s] -= ModT{1};
-        dif_rev_q[l][s + (1 << l)] = ModT{1};
-        if (l + 1 < log_m) {
-          NTT::dif(2 << l, dif_rev_q[l].data() + s);
+          dif_rev_q[l][s] -= ModT{1};
+          dif_rev_q[l][s + (1 << l)] = ModT{1};
         }
       }
     }
