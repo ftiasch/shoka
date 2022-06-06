5e934a525970851729d37c64df9db3f68ea6a526
Xiaoxu Guo <ftiasch0@gmail.com>
2022-06-06 10:13:43 +0800

    shorten

--- nd_array.h
+++ nd_array.h
@@ -3,14 +3,13 @@
 template <typename T, int... NS> struct NDArray;
 
 template <typename T, int N0, int... NS> struct NDArray<T, N0, NS...> {
-  using Nested = NDArray<T, NS...>;
-  using Array = std::array<typename Nested::Array, N0>;
+  using Array = std::array<typename NDArray<T, NS...>::Array, N0>;
 
-  static Array create() { return Array(); }
+  static Array create() { return {}; }
 };
 
 template <typename T, int N0> struct NDArray<T, N0> {
   using Array = std::array<T, N0>;
 
-  static Array create() { return Array(); }
+  static Array create() { return {}; }
 };
