f9b337cc718158d2e6d302a191a9bad07c81d5d0
Xiaoxu Guo <ftiasch0@gmail.com>
2023-01-29 16:33:59 +0800

    added many

--- test/poly_gen.hpp
+++ test/poly_gen.hpp
@@ -1,32 +1,67 @@
+#include <climits>
 #include <tuple>
 #include <vector>
 
-// TODO: min_deg
-// TODO: LazyMul
-// TODO: Cache
-template <typename Mod_, int ConstN, typename... Vs> struct ContextT {
-  using Mod = Mod_;
-  using Vector = std::vector<Mod>;
+template <typename Mod> struct ValueT {
+  explicit ValueT(const std::vector<Mod> &c_)
+      : c{c_}, min_deg{compute_min_deg(c)}, max_deg{static_cast<int>(c.size()) -
+                                                    1} {}
 
-  using CValue = Vector;
-  using CValues = std::array<CValue, ConstN>;
-
-  explicit ContextT(CValues &&cvalues_)
-      : cvalues{std::move(cvalues_)},
-        store{typename Vs::template StoreT<ContextT>{*this}...} {}
-
-  template <int Index> auto &var() { return std::get<Index>(store); }
-
-  template <int Index> auto const_at(int i) const {
-    const auto &c = cvalues[Index];
+  auto operator[](int i) const {
     return i < static_cast<int>(c.size()) ? c[i] : Mod{0};
   }
 
 private:
-  using Store = std::tuple<typename Vs::template StoreT<ContextT>...>;
+  static int compute_min_deg(const std::vector<Mod> &c) {
+    int deg = 0;
+    while (deg < static_cast<int>(c.size()) && c[deg] == Mod{0}) {
+      deg++;
+    }
+    return deg;
+  }
 
-  CValues cvalues;
-  Store store;
+  std::vector<Mod> c;
+
+public:
+  const int min_deg, max_deg;
+};
+
+template <typename Mod, int N> struct ValuesT {
+  using Is = std::array<std::vector<Mod>, N>;
+  using T = std::array<ValueT<Mod>, N>;
+
+  static T create(const Is &cvalues) {
+    return create(cvalues, std::make_index_sequence<N>());
+  }
+
+private:
+  template <std::size_t... Index>
+  static T create(const Is &cvalues, std::index_sequence<Index...>) {
+    return {ValueT<Mod>{cvalues[Index]}...};
+  }
+};
+
+// TODO: min_deg
+// TODO: LazyMul
+// TODO: Cache
+template <typename Mod_, int NUM_OF_VAL, typename... Vars> struct ContextT {
+  using Mod = Mod_;
+  using Vector = std::vector<Mod>;
+
+  using Values = ValuesT<Mod, NUM_OF_VAL>;
+
+  explicit ContextT(const typename Values::Is &cvalues_)
+      : cvalues{Values::create(cvalues_)},
+        store{typename Vars::template StoreT<ContextT>{*this}...} {}
+
+  template <int Index> auto &var() { return std::get<Index>(store); }
+
+  template <int Index> auto &val() const { return cvalues[Index]; }
+
+private:
+  typename Values::T cvalues;
+
+  std::tuple<typename Vars::template StoreT<ContextT>...> store;
 };
 
 // helper
@@ -52,47 +87,70 @@ template <int Index> struct Var {
 
     typename Ctx::Mod operator[](int i) { return ctx.template var<Index>()[i]; }
 
+  private:
     Ctx &ctx;
+
+  public:
+    const int min_deg = 0, max_deg = INT_MAX;
   };
 };
 
-template <int Index> struct Const {
+template <int Index> struct Val {
   template <typename Ctx> struct StoreT {
     static constexpr bool is_value = true;
 
     using Vector = typename Ctx::Vector;
 
-    explicit StoreT(Ctx &ctx_) : ctx{ctx_} {}
+    explicit StoreT(Ctx &ctx_)
+        : ctx{ctx_}, min_deg{val().min_deg}, max_deg{val().max_deg} {}
 
     using Mod = typename Ctx::Mod;
 
-    Mod operator[](int i) const { return ctx.template const_at<Index>(i); }
+    Mod operator[](int i) const { return val()[i]; }
 
   private:
+    auto &val() const { return ctx.template val<Index>(); }
+
     Ctx &ctx;
+
+  public:
+    const int min_deg, max_deg;
   };
 };
 
 template <typename P, typename Q> struct Add {
   template <typename Ctx> struct StoreT : public BinaryOpStoreT<Ctx, P, Q> {
-    using BinaryOpStoreT<Ctx, P, Q>::BinaryOpStoreT;
+    using Base = BinaryOpStoreT<Ctx, P, Q>;
+    using Base::p, Base::q;
 
-    auto operator[](int i) { return this->p[i] + this->q[i]; }
+    explicit StoreT(Ctx &ctx)
+        : Base{ctx}, min_deg{std::min(p.min_deg, q.min_deg)}, max_deg{std::max(
+                                                                  p.max_deg,
+                                                                  q.max_deg)} {}
+
+    auto operator[](int i) { return p[i] + q[i]; }
+
+    const int min_deg, max_deg;
   };
 };
 
 template <typename P, typename Q> struct Mul {
   template <typename Ctx> struct StoreT : public BinaryOpStoreT<Ctx, P, Q> {
-    using BinaryOpStoreT<Ctx, P, Q>::BinaryOpStoreT;
+    using Base = BinaryOpStoreT<Ctx, P, Q>;
+    using Base::p, Base::q;
+
+    explicit StoreT(Ctx &ctx)
+        : Base{ctx}, min_deg{p.min_deg + q.min_deg}, max_deg{INT_MAX} {}
 
     auto operator[](int k) {
       typename Ctx::Mod result{0};
-      // FIXME: min deg
-      for (int i = 0; i < k; ++i) {
-        result += this->p[i] * this->q[k - i];
+      for (int i = p.min_deg; i <= k - q.min_deg; ++i) {
+        result += p[i] * q[k - i];
       }
       return result;
     }
+
+    const int min_deg, max_deg;
   };
 };
 
@@ -105,18 +163,44 @@ template <typename P, typename Q> struct Mul {
 #include <catch2/catch_all.hpp>
 #include <catch2/generators/catch_generators.hpp>
 
-template <int Index> using C = Const<Index>;
+template <int Index> using C = Val<Index>;
 
 TEST_CASE("poly_gen") {
   using Mod = ModT<998'244'353>;
 
-  // f(z) = f(z) * z + 1
-  using GF = ContextT<Mod, 2, Add<Mul<Var<0>, Const<0>>, Const<1>>>;
+  auto take = [&](auto f, int n) {
+    std::vector<Mod> p(n);
+    for (int i = 0; i < n; ++i) {
+      p[i] = f[i];
+    }
+    return p;
+  };
 
-  GF ctx{{std::vector<Mod>{Mod{0}, Mod{1}}, {Mod{1}}}};
+  SECTION("geo_sum") {
+    // f(z) = f(z) * z + 1
+    using Ctx = ContextT<Mod, 2, Add<Mul<Var<0>, C<0>>, C<1>>>;
+    Ctx ctx{{std::vector<Mod>{Mod{0}, Mod{1}}, {Mod{1}}}};
+    auto &f = ctx.var<0>();
+    REQUIRE_FALSE(f.is_value);
+    REQUIRE(f[0] == Mod{1});
+    REQUIRE(f[1] == Mod{1});
+  }
 
-  auto &f = ctx.var<0>();
-  REQUIRE_FALSE(f.is_value);
-  REQUIRE(f[0] == Mod{1});
-  REQUIRE(f[1] == Mod{1});
+  SECTION("fib") {
+    // f(z) = f(z) * (z + z^2) + 1
+    using Ctx = ContextT<Mod, 2, Add<Mul<Var<0>, C<0>>, C<1>>>;
+    Ctx ctx{{std::vector<Mod>{Mod{0}, Mod{1}, Mod{1}}, {Mod{1}}}};
+    auto &f = ctx.var<0>();
+    REQUIRE(take(f, 5) ==
+            std::vector<Mod>{Mod{1}, Mod{1}, Mod{2}, Mod{3}, Mod{5}});
+  }
+
+  SECTION("catalan") {
+    // f(z) = f(z) * f(z) * z + 1
+    using Ctx = ContextT<Mod, 2, Add<Mul<Mul<Var<0>, Var<0>>, C<0>>, C<1>>>;
+    Ctx ctx{{std::vector<Mod>{Mod{0}, Mod{1}}, {Mod{1}}}};
+    auto &f = ctx.var<0>();
+    REQUIRE(take(f, 5) ==
+            std::vector<Mod>{Mod{1}, Mod{1}, Mod{2}, Mod{5}, Mod{14}});
+  }
 }
