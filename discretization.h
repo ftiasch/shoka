586ee7d0fbcf2d0b3b9ec69e7389eff307b8faea
Xiaoxu Guo <ftiasch0@gmail.com>
2023-02-04 16:03:28 +0800

    added normalize()

--- discretization.h
+++ discretization.h
@@ -1,14 +1,23 @@
 #include <algorithm>
 #include <vector>
 
-template <typename T> struct Discretization : public std::vector<T> {
+template <typename T, typename Compare = std::less<T>>
+struct Discretization : public std::vector<T> {
   using std::vector<T>::begin;
   using std::vector<T>::end;
 
+  explicit Discretization() = default;
+
   explicit Discretization(const std::vector<T> &a_) : std::vector<T>(a_) {
-    std::sort(begin(), end());
+    normalize();
+  }
+
+  void normalize() {
+    std::sort(begin(), end(), Compare{});
     std::vector<T>::erase(std::unique(begin(), end()), end());
   }
 
-  int index(T x) const { return std::lower_bound(begin(), end(), x) - begin(); }
+  int index(T x) const {
+    return std::lower_bound(begin(), end(), x, Compare{}) - begin();
+  }
 };
