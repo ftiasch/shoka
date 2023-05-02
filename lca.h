6906aee29dc459c349c70c4a8383ff9f805d7a96
Xiaoxu Guo <ftiasch0@gmail.com>
2023-05-02 12:53:57 +0800

    fixed

--- lca.h
+++ lca.h
@@ -1,15 +1,16 @@
-#include "y_combinator.h"
+#pragma once
 
+#include <functional>
 #include <vector>
 
-struct Lca {
-  explicit Lca(const std::vector<std::vector<int>> &tree, int root)
+struct Solver {
+  explicit Solver(const std::vector<std::vector<int>> &tree, int root)
       : n(tree.size()), max_d{get_max_d(n)}, depth(n),
         jump(n, std::vector<int>(max_d, -1)) {
-    y_combinator([&](auto dfs, int p, int u) -> void {
+    std::function<void(int, int)> dfs = [&](int p, int u) -> void {
       depth[u] = ~p ? depth[p] + 1 : 0;
       jump[u][0] = p;
-      for (int i = 0; ~jump[u][i] && i + 1 < max_d; ++i) {
+      for (int i = 0; ~jump[u][i] && i + 1 < max_d; i++) {
         jump[u][i + 1] = jump[jump[u][i]][i];
       }
       for (auto &&v : tree[u]) {
@@ -17,14 +18,15 @@ struct Lca {
           dfs(u, v);
         }
       }
-    })(-1, root);
+    };
+    dfs(-1, root);
   }
 
   int operator()(int x, int y) const {
     if (depth[x] > depth[y]) {
       std::swap(x, y);
     }
-    for (int d = 0; d < max_d; ++d) {
+    for (int d = 0; d < max_d; d++) {
       if ((depth[y] - depth[x]) >> d & 1) {
         y = jump[y][d];
       }
