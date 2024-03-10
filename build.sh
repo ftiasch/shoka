c4cfcf0e60f2c0692a25a210bbcd7d1693616bdd
Xiaoxu Guo <ftiasch0@gmail.com>
2024-03-10 15:02:48 +0800

    fixed conan2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o errexit
 
-conan install . -if Build -of Build --build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B Build .
+conan install . --output-folder=Build --build=missing
+(cd Build && cmake -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..)
 cmake --build Build
