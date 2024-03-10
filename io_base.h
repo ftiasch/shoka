97e9397f35ced4f62601eb64dd30cec1a7548684
Xiaoxu Guo <ftiasch0@gmail.com>
2024-03-10 16:03:45 +0800

    simplified IOBase

--- io_base.h
+++ io_base.h
@@ -1,11 +1,7 @@
 #pragma once
 
-#include "is_specialization_of.h"
-
 #include <experimental/type_traits>
-#include <string>
 #include <tuple>
-#include <type_traits>
 #include <vector>
 
 #ifndef YES
@@ -16,18 +12,24 @@
 #endif
 
 template <typename IO> struct IOBaseT {
-  template <typename T = int> T read(T &&v = T{}) {
-    using DecayedT = std::decay_t<T>;
-    if constexpr (is_specialization_of_v<std::tuple, DecayedT>) {
-      read_t_(v, std::make_index_sequence<std::tuple_size_v<DecayedT>>());
-    } else if constexpr (is_vector_like<DecayedT>()) {
-      for (auto it = v.begin(); it != v.end(); it++) {
-        read(*it);
-      }
-    } else {
-      static_cast<IO *>(this)->template read1(std::forward<T>(v));
-    }
+  template <typename T = int> T read() {
+    T v;
+    static_cast<IO *>(this)->template read1(v);
     return v;
+  }
+
+  template <typename T = int> std::vector<T> read_v(size_t n) {
+    std::vector<T> vec(n);
+    for (auto &&v : vec) {
+      v = read<T>();
+    }
+    return vec;
+  }
+
+  template <typename... Ts> std::tuple<Ts...> read_t() {
+    std::tuple<Ts...> t;
+    read_t_(t, std::make_index_sequence<sizeof...(Ts)>());
+    return t;
   }
 
   IOBaseT &operator<<(bool o) { return *this << (o ? YES : NO); }
@@ -52,26 +54,9 @@ template <typename IO> struct IOBaseT {
     return static_cast<IO *>(this)->template write1(std::forward<T>(o)), *this;
   }
 
-  // helper
-
-  template <typename T = int> std::vector<T> read_v(int n) {
-    return read(std::vector<T>(n));
-  }
-
-  template <typename... Ts> std::tuple<Ts...> read_t() {
-    return read(std::tuple<Ts...>{});
-  }
-
 private:
-  template <typename T> using has_begin_t = decltype(std::declval<T>().begin());
-
-  template <typename T> static constexpr bool is_vector_like() {
-    return !std::is_same_v<T, std::string> &&
-           std::experimental::is_detected_v<has_begin_t, T>;
-  }
-
   template <typename Tuple, std::size_t... Index>
   void read_t_(Tuple &t, std::index_sequence<Index...>) {
-    (..., (std::get<Index>(t) = read(std::tuple_element_t<Index, Tuple>{})));
+    (..., (std::get<Index>(t) = read<std::tuple_element_t<Index, Tuple>>()));
   }
 };
--- test/io.hpp
+++ test/io.hpp
@@ -19,17 +19,12 @@ TEST_CASE("fast_io") {
     REQUIRE(io.read() == -2147483647);
     REQUIRE(io.read() == 0);
     REQUIRE(io.read() == 2147483647);
-    REQUIRE(io.read((long long){}) == -9223372036854775807);
-    REQUIRE(io.read((long long){}) == 0);
-    REQUIRE(io.read((long long){}) == 9223372036854775807);
+    REQUIRE(io.read<long long>() == -9223372036854775807);
+    REQUIRE(io.read<long long>() == 0);
+    REQUIRE(io.read<long long>() == 9223372036854775807);
 
-    REQUIRE(io.read(std::tuple<int, int>{}) == std::tuple<int, int>{0, 1});
-    REQUIRE(io.read(std::vector<int>(3)) == std::vector<int>{2, 3, 4});
-    REQUIRE(io.read(std::vector<std::vector<int>>(2, std::vector<int>(3))) ==
-            std::vector<std::vector<int>>{{5, 6, 7}, {8, 9, 10}});
-    REQUIRE(io.read_t<int, int, int>() ==
-            std::tuple<int, int, int>{11, 12, 13});
-    REQUIRE(io.read_v(2) == std::vector<int>{14, 15});
+    REQUIRE(io.read_t<int, int>() == std::tuple<int, int>{0, 1});
+    REQUIRE(io.read_v(3) == std::vector<int>{2, 3, 4});
 
     std::fclose(tmpf);
   }
