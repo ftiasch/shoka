e1414c97f85c16fe175a093602ba9c66c6c11b0d
Xiaoxu Guo <shimo11370@proton.me>
2024-04-29 02:35:01 +0800

    updated

--- sa_doubling.h
+++ sa_doubling.h
@@ -18,30 +18,27 @@ suffix_array(const std::vector<int> &s) {
     order[--count[s[i]]] = i;
   }
   for (int k = 0; 1 << k < n; k++) {
-    std::ranges::fill(count, 0);
-    for (int i = 0; i < n; i++) {
-      count[rank[i]]++;
+    for (int i = n - (1 << k); i < n; i++) {
+      new_order[count[rank[i]]++] = i;
     }
-    for (int i = 1; i < n; i++) {
-      count[i] += count[i - 1];
-    }
-    for (int i_ : order | std::views::reverse) {
-      int i = i_ - (1 << k);
+    for (int j : order) {
+      int i = j - (1 << k);
       if (i >= 0) {
-        new_order[--count[rank[i]]] = i;
+        new_order[count[rank[i]]++] = i;
       }
-    }
-    for (int i = n; i-- > n - (1 << k);) {
-      new_order[--count[rank[i]]] = i;
     }
     order.swap(new_order);
     int cur{-1};
-    std::pair<int, int> last_p{-1, -1};
-    for (int i : order) {
+    std::pair<int, int> lsp{-1, -1};
+    for (int j = 0; j < n; j++) {
+      int i = order[j];
       std::pair<int, int> p{rank[i],
                             i + (1 << k) < n ? rank[i + (1 << k)] : -1};
-      new_rank[i] = (cur += last_p != p);
-      last_p = p;
+      if (lsp != p) {
+        count[++cur] = j;
+        lsp = p;
+      }
+      new_rank[i] = cur;
     }
     rank.swap(new_rank);
   }
