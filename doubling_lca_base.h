b438ca3c650dd7f6aa5aa788cfda9e71b6bdace7
Xiaoxu Guo <shimo11370@proton.me>
2024-06-08 07:29:14 +0800

    extracted doubling_lca_base

---
+++ doubling_lca_base.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "snippets/min_pow_of_two.h"
+
+#include <vector>
+
+class DoublingLcaBase {
+  int n, max_d;
+  std::vector<int> depth;
+  std::vector<std::vector<int>> jump;
+
+public:
+  explicit DoublingLcaBase(int n_)
+      : n(n_), max_d{min_pow_of_two(n)}, depth(n),
+        jump(n, std::vector<int>(max_d, -1)) {}
+
+  void add_leaf(int u, int p) {
+    depth[u] = depth[p] + 1;
+    jump[u][0] = p;
+    for (int i = 0; ~jump[u][i] && i + 1 < max_d; i++) {
+      jump[u][i + 1] = jump[jump[u][i]][i];
+    }
+  }
+
+  int lca(int x, int y) const {
+    if (depth[x] > depth[y]) {
+      std::swap(x, y);
+    }
+    for (int d = 0; d < max_d; d++) {
+      if ((depth[y] - depth[x]) >> d & 1) {
+        y = jump[y][d];
+      }
+    }
+    if (x == y) {
+      return x;
+    }
+    for (int d = max_d; d--;) {
+      if (jump[x][d] != jump[y][d]) {
+        x = jump[x][d];
+        y = jump[y][d];
+      }
+    }
+    return jump[x][0];
+  }
+};
--- lca.h
+++ lca.h
@@ -1,18 +1,14 @@
 #pragma once
 
-#include "snippets/min_pow_of_two.h"
+#include "doubling_lca_base.h"
 #include "types/graph.h"
-
-#include <vector>
 
 template <IsGraph Graph>
   requires std::is_same_v<GraphEdge<Graph>, int>
-class Lca {
+class Lca : public DoublingLcaBase {
   void dfs(int p, int u) {
-    depth[u] = ~p ? depth[p] + 1 : 0;
-    jump[u][0] = p;
-    for (int i = 0; ~jump[u][i] && i + 1 < max_d; i++) {
-      jump[u][i + 1] = jump[jump[u][i]][i];
+    if (~p) {
+      add_leaf(u, p);
     }
     for (auto &&v : tree[u]) {
       if (v != p) {
@@ -22,35 +18,12 @@ class Lca {
   };
 
   const Graph &tree;
-  int n, max_d;
-  std::vector<int> depth;
-  std::vector<std::vector<int>> jump;
 
 public:
   explicit Lca(const Graph &tree_, int root)
-      : tree{tree_}, n(tree.size()), max_d{min_pow_of_two(n)}, depth(n),
-        jump(n, std::vector<int>(max_d, -1)) {
+      : DoublingLcaBase(tree_.size()), tree{tree_} {
     dfs(-1, root);
   }
 
-  int operator()(int x, int y) const {
-    if (depth[x] > depth[y]) {
-      std::swap(x, y);
-    }
-    for (int d = 0; d < max_d; d++) {
-      if ((depth[y] - depth[x]) >> d & 1) {
-        y = jump[y][d];
-      }
-    }
-    if (x == y) {
-      return x;
-    }
-    for (int d = max_d; d--;) {
-      if (jump[x][d] != jump[y][d]) {
-        x = jump[x][d];
-        y = jump[y][d];
-      }
-    }
-    return jump[x][0];
-  }
+  int operator()(int a, int b) { return lca(a, b); }
 };
